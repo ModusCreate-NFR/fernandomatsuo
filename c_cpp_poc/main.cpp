d1c0b9f470b803d042d0c91622d6a86ac1d91300
Fernando Matsuo Santos <fernando@familiamatsuo.com>
2024-11-12 14:05:30 -0300

    start repo

--- c_cpp_poc/main.cpp
+++ c_cpp_poc/main.cpp
@@ -1,12 +1,25 @@
+// #include <iostream>
+
+// int main(int argc, char** argv){
+
+// 	std::cout << "Hello World" << std::endl;
+// 	return 0;
+// }
+
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv){
+void queryDatabase(const std::string& userInput) {
+    std::string query = "SELECT * FROM users WHERE name = '" + userInput + "';"; 
+    // Potential SQL injection vulnerability
+    std::cout << "Executing query: " << query << std::endl;
+    // ...execute query (not shown)
+}
 
-	int uninitializedVar;  // Uninitialized variable usage
-    if (uninitializedVar == 0) {  // Potential undefined behavior
-        std::cout << "Variable is zero" << std::endl;
-    }
-
-	std::cout << "Hello World" << std::endl;
-	return 0;
+int main() {
+    std::string userInput;
+    std::cout << "Enter your name: ";
+    std::cin >> userInput;
+    queryDatabase(userInput);
+    return 0;
 }
