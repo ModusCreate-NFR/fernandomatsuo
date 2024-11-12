3baf0876bd9f87046f8bbaee24b4f53fcffa0937
Fernando Matsuo Santos <fernando@familiamatsuo.com>
2024-11-12 14:15:14 -0300

    start repo

--- c_cpp_poc/main.cpp
+++ c_cpp_poc/main.cpp
@@ -1,25 +1,12 @@
-// #include <iostream>
-
-// int main(int argc, char** argv){
-
-// 	std::cout << "Hello World" << std::endl;
-// 	return 0;
-// }
-
 #include <iostream>
-#include <string>
 
-void queryDatabase(const std::string& userInput) {
-    std::string query = "SELECT * FROM users WHERE name = '" + userInput + "';"; 
-    // Potential SQL injection vulnerability
-    std::cout << "Executing query: " << query << std::endl;
-    // ...execute query (not shown)
+int main(int argc, char** argv){
+
+	std::cout << "Hello World" << std::endl;
+	return 0;
 }
 
-int main() {
-    std::string userInput;
-    std::cout << "Enter your name: ";
-    std::cin >> userInput;
-    queryDatabase(userInput);
-    return 0;
+bool checkOverflow(unsigned short x, unsigned short y) {
+  // BAD: comparison is always false due to type promotion
+  return (x + y < x);  
 }
