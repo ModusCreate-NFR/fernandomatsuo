13ff8b2c149b421e91cb3db86d0642c497fe6865
Fernando Matsuo Santos <fernando@familiamatsuo.com>
2024-11-12 14:22:11 -0300

    CodeQL issue

--- c_cpp_poc/main.cpp
+++ c_cpp_poc/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv){
 	return 0;
 }
 
-// // Issue to be detected by CodeQL
-// bool checkOverflow(unsigned short x, unsigned short y) {
-//   // BAD: comparison is always false due to type promotion
-//   return (x + y < x);  
-// }
+// Issue to be detected by CodeQL
+bool checkOverflow(unsigned short x, unsigned short y) {
+  // BAD: comparison is always false due to type promotion
+  return (x + y < x);  
+}
